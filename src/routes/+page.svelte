791550e364a204e3cb72005ce5cea365b16f73a3
manuelJung <manuel.jung.wwi12@gmail.com>
2023-07-13 12:56:51 +0200

    reset to basic code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,6 @@
 <script lang="ts">
-  import Foo from "../components/Foo.svelte";
-
-  let show = true
-
-  console.log('parent')
+  
 
 </script>
 
-<button on:click={() => show=!show}>toggle Foo</button>
-
-<div>
-  {#if show}
-    <Foo/>
-  {/if}
-  <!-- <Foo/> -->
-</div>
+<p>Hello World</p>
